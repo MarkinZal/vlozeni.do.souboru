94e72c975f57a8652c6ef2b292422419fa6d9812
MarkinZal <148867206+MarkinZal@users.noreply.github.com>
2023-11-28 12:23:05 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,22 @@ import static java.lang.System.out;
 public class Main {
     public static void main(String[] args) {
 
-
         String nazevSouboru = "vystupniSoubor.txt";
+        FileWriter fw;
+        {
+            try {
+                fw = new FileWriter(nazevSouboru, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                String textKZapsani = "Hello there...";
+                bw.write(textKZapsani);
+                bw.newLine();
+                bw.close();
+                fw.close();
+                System.out.println("Text byl zapsán do souboru.");
 
-        try {
-            FileWriter fileWriter = new FileWriter(nazevSouboru, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            String textKZapsani = "Hello there...";
-            bufferedWriter.write(textKZapsani);
-            bufferedWriter.newLine();
-            bufferedWriter.close();
-            fileWriter.close();
-            System.out.println("Text byl zapsán do souboru.");
-
-        } catch (IOException e) {
-            System.out.println("Chyba při zápisu do souboru: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Chyba při zápisu do souboru: " + e.getMessage());
+            }
         }
     }
 }
